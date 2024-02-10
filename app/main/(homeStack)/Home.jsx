75a15459b3c53c98c95862364cfc033d3ae0eeec
NadimChibani <npc00@mail.aub.edu>
2024-02-10 17:28:09 +0200

    installing extra package before fixing

--- app/main/(homeStack)/Home.jsx
+++ app/main/(homeStack)/Home.jsx
@@ -1,16 +1,23 @@
 import React, { useEffect } from 'react'
-import { Text, BackHandler, Alert } from 'react-native'
+import { Text, BackHandler, Alert, View, Button } from 'react-native'
 import { SafeAreaView } from "react-native-safe-area-context";
 import CustomHeader from '../../../components/CustomHeader'
+import Carousel from 'react-native-snap-carousel';
 
 const Page = () => {
-
+  const newsData = [
+    // Your news data here
+    // Example: { title: 'Breaking News', content: 'Lorem ipsum...' }
+];
+const foodCategories = [
+  'French', 'Italian', 'Chinese', 'Arabic', 'Japanese' // Add more as needed
+];
 
   return (
     <SafeAreaView>
       <CustomHeader/>
+
     </SafeAreaView>
-    
   )
 }
 
