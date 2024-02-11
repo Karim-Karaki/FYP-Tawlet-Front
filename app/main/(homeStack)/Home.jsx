ceea1b3784b7dfff4729f42e76dc1a77ce665d9a
NadimChibani <npc00@mail.aub.edu>
2024-02-11 22:47:52 +0200

    Adding carousel base testing

--- app/main/(homeStack)/Home.jsx
+++ app/main/(homeStack)/Home.jsx
@@ -1,9 +1,10 @@
 import React, { useEffect } from 'react'
-import { Text, BackHandler, Alert, View, Button, StyleSheet  } from 'react-native'
+import { Text, BackHandler, Alert, View, Button, StyleSheet, Dimensions  } from 'react-native'
 import { SafeAreaView } from "react-native-safe-area-context";
 import CustomHeader from '../../../components/CustomHeader'
 import Carousel from 'react-native-snap-carousel';
-
+import { KeyboardAvoidingScrollView } from "react-native-keyboard-avoiding-scroll-view";
+import colors from "../../../constants/colors";
 const Page = () => {
   const newsData = [
     // Your news data here
@@ -12,46 +13,69 @@ const Page = () => {
 const foodCategories = [
   'French', 'Italian', 'Chinese', 'Arabic', 'Japanese' // Add more as needed
 ];
-
-  return (
-    <SafeAreaView>
+const staticData = [
+  { id: 1, title: 'Item 1' },
+  { id: 2, title: 'Item 2' },
+  { id: 3, title: 'Item 3' },
+];
+const renderItem = ({ item }) => {
+  return(
+  <View style={{ backgroundColor: 'lightblue', padding: 20 }}>
+      <Text>{item.title}</Text>
+  </View>
+  );
+};
+  return ( 
+    // <KeyboardAvoidingScrollView style={{backgroundColor: colors.background, flex: 1}} contentContainerStyle={{ flexGrow: 1 }}>
+    <SafeAreaView style={styles.container}>  
       <CustomHeader/>
-      
-      <View style={styles.container}>
-            {/* News Carousel */}
-            {/* <Carousel
-                data={newsData}
-                renderItem={({ item }) => (
-                    <View style={styles.newsItem}>
-                        <Text>{item.title}</Text>
-                        <Text>{item.content}</Text>
-                    </View>
-                )}
-                sliderWidth={400}
-                itemWidth={300}
-            /> */}
-
-            {/* Food Category Buttons */}
-            {/* <View style={styles.categoryButtons}>
-                {foodCategories.map(category => (
-                    <Button
-                        key={category}
-                        title={category}
-                        onPress={() => {
-                            // Handle button press (e.g., navigate to category screen)
-                        }}
-                    />
-                ))}
-            </View> */}
-        </View>
+      <View style={{backgroundColor:"green"}}>
+      <Carousel
+      data={staticData}
+      renderItem={renderItem}
+      sliderWidth={Dimensions.get('window').width}
+      itemWidth={300}
+      />
+      </View>
     </SafeAreaView>
+    // </KeyboardAvoidingScrollView>
+
   )
 };
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    // paddingHorizontal: 20 ,
+    flexDirection: 'column',
+  },
+  overlay: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    zIndex: 0,
+    backgroundColor: 'rgba(255, 0, 0, 0.5)', // Semi-transparent red overlay
+},
+  header: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'lightblue',
+  },
+  content: {
+      flex: 2,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: 'lightgreen',
+  },
+  footer: {
       flex: 1,
-      backgroundColor: '#456783',
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: 'lightcoral',
   },
   newsItem: {
       // Style your news item here
