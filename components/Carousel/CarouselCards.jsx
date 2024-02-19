f310890b665de06eff3e3ca5024a5b60f8651fe8
NadimChibani <npc00@mail.aub.edu>
2024-02-19 05:07:10 +0200

    Added Pagination to carousel

--- components/Carousel/CarouselCards.jsx
+++ components/Carousel/CarouselCards.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { View, StyleSheet } from "react-native"
-import Carousel from 'react-native-snap-carousel'
+import Carousel, { Pagination }from 'react-native-snap-carousel'
 import CarouselCardItem, { SLIDER_WIDTH, ITEM_WIDTH } from './CarouselCardItem'
 import data from './data'
 
 const CarouselCards = () => {
   const isCarousel = React.useRef(null)
+  const [index, setIndex] = React.useState(0)
 
   return (
     <View >
@@ -18,7 +19,23 @@ const CarouselCards = () => {
         sliderWidth={SLIDER_WIDTH}
         itemWidth={ITEM_WIDTH}
         inactiveSlideShift={0}
+        onSnapToItem={(index) => setIndex(index)}
         useScrollView={true}
+      />
+      <Pagination
+        dotsLength={data.length}
+        activeDotIndex={index}
+        carouselRef={isCarousel}
+        dotStyle={{
+          width: 10,
+          height: 10,
+          borderRadius: 5,
+          marginHorizontal: 0,
+          backgroundColor: 'rgba(0, 0, 0, 0.92)'
+        }}
+        inactiveDotOpacity={0.4}
+        inactiveDotScale={0.6}
+        tappableDots={true}
       />
     </View>
   )
