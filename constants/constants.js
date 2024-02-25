52090d28d7601c3da0035bbd668762a03ae65846
NadimChibani <npc00@mail.aub.edu>
2024-02-25 21:21:09 +0200

    changed source

--- constants/constants.js
+++ constants/constants.js
@@ -50,7 +50,7 @@ export const featured = {
         {
             id: 1,
             name: 'Papa Johns',
-            image: require('../assets/images/bike.png'),
+            image: require('../assets/images/restaurant.png'),
             description: 'Hot and spicy pizzas',
             lng: -85.5324269,
             lat: 38.2145602,
@@ -86,7 +86,7 @@ export const featured = {
         {
             id: 2,
             name: 'Papa Johns',
-            image: require('../assets/images/bike.png'),
+            image: require('../assets/images/restaurant.png'),
             description: 'Hot and spicy pizzas',
             lng: -85.5324269,
             lat: 38.2145602,
@@ -122,7 +122,7 @@ export const featured = {
         {
             id: 3,
             name: 'Papa Johns',
-            image: require('../assets/images/bike.png'),
+            image: require('../assets/images/restaurant.png'),
             description: 'Hot and spicy pizzas',
             lng: -85.5324269,
             lat: 38.2145602,
